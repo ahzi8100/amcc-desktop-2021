9c048347418def924e0a41d1f94b91aead5f0334
ahzi8100 <uzif8100@students.amikom.ac.id>
2022-01-15 17:12:28 +0700

    File Pertemuan ke-2

--- index.py
+++ index.py
@@ -1,1 +1,172 @@
-print("Hello World!")
+# Komentar nya
+
+"""
+Ini saya ketik lebih dari 1
+ini baris kedua
+ini baris ketiga
+"""
+
+# a = 'Selamat Datang'  # String
+# b = 342  # integer
+# c = 23.4  # Float
+# d = False  # Boolean
+
+# # print(a)
+# # print(d)
+
+# Operator Artimatika
+
+# a = 5
+# b = 3
+
+# # penjumlahan
+# hasil = a + b
+# print("5 + 3 = ", hasil)
+
+# # pengurangan
+# hasil = a - b
+# print("5 - 3 = ", hasil)
+
+# # perkalian
+# hasil = a * b
+# print("5 * 3 = ", hasil)
+
+# # pembagian
+# hasil = a / b
+# print("5 / 3 = ", hasil)
+
+# # sisa hasil bagi
+# hasil = a % b
+# print("5 % 3 = ", hasil)
+
+# # pangkat
+# hasil = a ** b
+# print("5 ** 3 = ", hasil)
+
+# # Pembagian Bulat
+# hasil = a // b
+# print("5 // 3 = ", hasil)
+
+# angka1 = 12
+# angka2 = 15.23
+
+# hasil = angka1 + angka2
+# print(hasil)
+
+
+# Operator Perbandingan
+
+
+# == - sama dengan
+# != - Tidak sama dengan
+# <> - Tidak sama dengan
+# > -  Lebih Besar dari
+# < - Kurang dari
+# >= - Lebih besar sama dengan
+# <= - Kurang dari sama dengan
+
+# a = 2
+# b = 2
+
+# # Sama dengan
+# hasil = a == b
+# print(a, " == ", b, " = ", hasil)
+
+# # <> - Tidak sama dengan
+# hasil = a != b
+# print(a, " != ", b, " = ", hasil)
+
+# # lebih besar dari
+# hasil = a > b
+# print(a, " > ", b, " = ", hasil)
+
+# # lebih kecil dari
+# hasil = a < b
+# print(a, " < ", b, " = ", hasil)
+
+# # Lebih besar sama dengan
+# hasil = a >= b
+# print(a, " >= ", b, " = ", hasil)
+
+# # lebih kecil sama dengan
+# hasil = a <= b
+# print(a, " <= ", b, " = ", hasil)
+
+# Operator Penugasan
+# a = 1 - Memberikan / Memasukan nilai ke variable
+# a += 1 # tambah sesuai nilai variable itu sendiri
+# a -= 1 # dikurang sesuai nilai variable itu sendiri
+# a *= 1 # dikali sesuai nilai variable itu sendiri
+# a /= 1 # dibagi sesuai nilai variable itu sendiri
+# a %= 1 # sisa bagi sesuai nilai variable itu sendiri
+# a **= 1 # dipangkat sesuai nilai variable itu sendiri
+# a //= 1 # disisa hasil bagi sesuai nilai variable itu sendiri
+
+# Operator Logical
+# and
+# hasil = (5 > 6) and (10 <= 8)
+# print(hasil)
+
+# # or
+# hasil = (5 < 6) or (10 <= 8)
+# print(hasil)
+
+# # not
+# hasil = not(10 < 10)
+# print(hasil)
+
+# a = "5"
+# b = "8"
+
+# # output True
+# print('a is b:', a is b)
+
+# # output False
+# print('a is not b:', a is not b)
+
+# # if
+# Diterima = 'No'
+# if Diterima == 'No':
+#     print('Sudah Jangan Sedih Yah')
+
+# # if else
+# jumlah = int(input('Berapa Jumlah Doi Kamu?: '))
+# if jumlah >= 4:
+#     print('Kamu Fucekboy ah')
+# else:
+#     print('Gini Dong Setia')
+
+
+# if elif else
+# nilai = int(input('Berapa Nilai Kamu?: '))
+# if nilai >= 81:
+#     Grade = 'A'
+# elif nilai >= 61:
+#     Grade = 'B'
+# elif nilai >= 51:
+#     Grade = 'C'
+# elif nilai >= 41:
+#     Grade = 'D'
+# else:
+#     Grade = 'E'
+
+# print("Nilai Kamu adalah ", Grade)
+
+# Perulangan for
+# ulang = int(input('Berapa kali diulang?: '))
+# for i in range(ulang):
+#     print('perulangan ke-'+str(i+1))
+
+buah = ['Jeruk', "Mangga", 'Semangka']
+for x in buah:
+    print(x)
+
+
+# jawab = 'ya'
+# hitung = 0
+
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input('Ulang lagi tidak?: ')
+
+# print('Total perulangan: '+str(hitung))
